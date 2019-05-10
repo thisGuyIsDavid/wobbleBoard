05a5c5dfd538f7d36dc708112989374c8b29179d
David Haverberg <dhaverberg@medicallay.org>
2019-05-09 20:19:34 -0500

    status light

--- pi_status.py
+++ pi_status.py
@@ -1,10 +1,12 @@
 import RPi.GPIO as GPIO
+import time
 
 try:
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
 	GPIO.output(23, GPIO.HIGH)
 	while True:
+		time.sleep(1)
 		pass
 except KeyboardInterrupt:
 	pass
