492569825266d85bfe7f1935983a4f0a62e5f422
David Haverberg <dhaverberg@medicallay.org>
2019-05-09 20:13:54 -0500

    status light

---
+++ pi_status.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+
+try:
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.output(23, GPIO.HIGH)
+except KeyboardInterrupt:
+	pass
+finally:
+	GPIO.output(23, GPIO.LOW)
+	GPIO.cleanup()
