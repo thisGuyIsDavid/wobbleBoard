48025f00855bccde066fa604aa03b854c72dcdac
David Haverberg <dhaverberg@medicallay.org>
2019-05-09 20:28:04 -0500

    gpio

--- pi_status.py
+++ pi_status.py
@@ -4,12 +4,14 @@ import subprocess
 try:
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
-	GPIO.setup(25, GPIO.IN)
+	GPIO.setup(25, GPIO.IN, initial=GPIO.LOW)
 
 	GPIO.output(23, GPIO.HIGH)
 	while True:
-		if not GPIO.input(25):  # if port 25 == 1
-			subprocess.call(['shutdown', '-h', 'now'], shell=False)
+		if GPIO.input(25):  # if port 25 == 1
+			print("Port 25 is 1/HIGH/True - LED ON")
+			#				subprocess.call(['shutdown', '-h', 'now'], shell=False)
+
 		time.sleep(1)
 except KeyboardInterrupt:
 	pass
