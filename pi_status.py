bece1eed68852b64f142d96f8f7e6a4ce5584a36
David Haverberg <dhaverberg@medicallay.org>
2019-05-09 20:24:37 -0500

    button

--- pi_status.py
+++ pi_status.py
@@ -4,12 +4,16 @@ import time
 try:
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(25, GPIO.IN)
+
 	GPIO.output(23, GPIO.HIGH)
 	while True:
+		if GPIO.input(25):  # if port 25 == 1
+			print("Port 25 is 1/HIGH/True - LED ON")
 		time.sleep(1)
-		pass
 except KeyboardInterrupt:
 	pass
 finally:
+	GPIO.output(25, GPIO.LOW)
 	GPIO.output(23, GPIO.LOW)
 	GPIO.cleanup()
