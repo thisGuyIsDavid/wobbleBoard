bc29bcc1a4a9d399c71a08e7dedf1b636a78747b
David Haverberg <dhaverberg@medicallay.org>
2019-05-09 20:38:08 -0500

    changes

--- pi_status.py
+++ pi_status.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
-
+import time
 #				subprocess.call(['shutdown', '-h', 'now'], shell=False)
 
 try:
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
 	GPIO.output(23, GPIO.HIGH)
+	while True:
+		time.sleep(5)
 except KeyboardInterrupt:
 	pass
 finally:
