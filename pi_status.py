75e4fac061deee23fe1621e4c2688f69b014a975
David Haverberg <dhaverberg@medicallay.org>
2019-05-09 20:26:47 -0500

    button

--- pi_status.py
+++ pi_status.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-
+import subprocess
 try:
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
@@ -9,7 +9,7 @@ try:
 	GPIO.output(23, GPIO.HIGH)
 	while True:
 		if not GPIO.input(25):  # if port 25 == 1
-			print("Port 25 is 1/HIGH/True - LED ON")
+			subprocess.call(['shutdown', '-h', 'now'], shell=False)
 		time.sleep(1)
 except KeyboardInterrupt:
 	pass
