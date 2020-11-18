736431d3582bd032074897e3f8a33db8e28ac1c9
David <dhaverberg@medicalalley.org>
2020-11-17 18:12:29 -0600

    testing print

--- main.py
+++ main.py
@@ -82,7 +82,6 @@ class WobbleReader:
 
             #   set keys
             key = "%s_%s_%s" % (x_rotation, y_rotation, z_rotation)
-            print(key)
             if self.last_key == key:
                 continue
             else:
